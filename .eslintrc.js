17b22eff9348066ac93be2e00f011138f35295a6
Ajay Singh Panwar <a.panwar48656@gmail.com>
2023-11-05 03:04:53 +0530

    Setting up eslint prettier and husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,69 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+    "react-native/react-native": true
+  },
+  extends: [
+    '@react-native',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier'
+  ],
+  overrides: [
+    {
+      'env': {
+        'node': true
+      },
+      'files': ['*.ts', '*.tsx'],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  parser: '@typescript-eslint/parser',
+  'parserOptions': {
+    project: './tsconfig.json',
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    "parserOptions": {
+      "ecmaFeatures": {
+        "jsx": true
+      }
+    }
+  },
+  plugins: ['react', 'react-native', 'react-hooks', '@typescript-eslint', "eslint-plugin-import"],
+  'rules': {
+    'prettier/prettier': 'error',
+    indent: ['error', 2, { SwitchCase: 1 }],
+    quotes: ['error', 'single', { avoidEscape: true }],
+    'semi': [
+      'error',
+      'always'
+    ],
+    // Typescript rules
+    'no-empty-function': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    "no-use-before-define": "off",
+    "@typescript-eslint/no-use-before-define": ["error"],
+    // React rules
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+    "react-hooks/rules-of-hooks": 'error',
+    "react-hooks/exhaustive-deps": "off",
+    // React native rules
+    "react-native/no-unused-styles": 'error',
+    "react-native/no-inline-styles": 'off',
+    "react-native/no-single-element-style-arrays": 'warn'
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
--- src/container/Home.tsx
+++ src/container/Home.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import {AppText} from '../components/AppText';
 import Theme from '../components/Theme';
 import {View} from 'react-native';
+import {TextBase} from 'react-native';
 
 const Home = () => {
+  let a = 10;
+
   return (
     <Theme isAreaInsets containerStyle={{backgroundColor: 'red'}}>
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
